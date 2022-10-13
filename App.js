14ac288474adfa7d2039001ac905138166800245
GTKoning <tom@koning-devos.nl>
2022-10-13 09:30:02 +0200

    Trying to get the background to work

--- App.js
+++ App.js
@@ -3,18 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>BICARE</Text>
-      <Text>Barter your bike repair anywhere</Text>
-      <StatusBar style="auto" />
+    <View style={styles.ladida}>
+      <View style={styles.container}>
+      
+        <Text>BICARE</Text>
+        <Text>Barter your bike repair anywhere</Text>
+        <StatusBar style="auto" />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
+  ladida: {
+    backgroundColor: 'linear-gradient(#751A33, #B34233)',
+    backgroundSize: 'cover',
+    height: "100%",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'blue',
     alignItems: 'center',
     justifyContent: 'center',
   },
