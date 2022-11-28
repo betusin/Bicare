bf3ed4172dc2bf8da0d8e8b518806c59675802f4
GTKoning <tom@koning-devos.nl>
2022-11-28 17:14:20 +0100

    Trying to make a global stylesheet, attempt 1

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import SignupScreen from "./screens/SignupScreen";
 import { useFonts } from 'expo-font';
 const Stack = createStackNavigator();
 
+
 export default function App() {
 
   return (
@@ -28,47 +29,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  title: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 42,
-    fontFamily: "Roboto",
-  },
-  text: {
-    color: "white",
-    fontSize: 26,
-    fontWeight: "300",
-    fontFamily: "Roboto",
-  },
-  ladida: {
-    backgroundImage: "linear-gradient(#751A33, #B34233)",
-    backgroundSize: "cover",
-    height: "100%",
-  },
-  container: {
-    flex: 0.3,
-    backgroundColor: "transparent",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    flex: 0.3,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: "5%",
-  },
-  inputWrapper: {
-    //flex: 0.3,
-    //backgroundColor: "white",
-    alignItems: "center",
-
-    //justifyContent: "center",
-  },
-  space: {
-    width: 20, // or whatever size  you need
-    height: 40,
-  },
-});
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -9,9 +9,10 @@ import {
 } from "react-native";
 import { LinearGradient } from 'expo-linear-gradient';
 import React, { useState } from "react";
-import {TouchableOpacity} from "react-native";
+import {TouchableOpacity} from "react-native";            
 import { useFonts } from 'expo-font';
 import SignupScreen from "./SignupScreen";
+import page from '../styles'
 export default function LoginScreen({ navigation }) {
   const [usernameText, setTextUsername] = useState("");
   const [passwordText, setTextPassword] = useState("");
@@ -20,160 +21,53 @@ export default function LoginScreen({ navigation }) {
     'Roboto': require('./../assets/fonts/Roboto-Regular.ttf'),
   });
 
+
   return (
 	<LinearGradient colors={['#751A33', '#B34233']} style={{flex:1}} locations={[0.0, 1.0]}>
-    <SafeAreaView style={styles.container}>
-      <View style={styles.ladida}>
-        <Text style={styles.title}>BICARE</Text>
-        <Text style={styles.subtitle}>Barter your bike repair anywhere</Text>
-        <View style={styles.inputWrapper}>
+    <SafeAreaView style={page.container}>
+      <View style={page.ladida}>
+        <Text style={page.title}>BICARE</Text>
+        <Text style={page.subtitle}>Barter your bike repair anywhere</Text>
+        <View style={page.inputWrapper}>
           <TextInput
-            style={styles.input}
+            style={page.input}
             placeholder="Enter your Username"
             onChangeText={(newText) => setTextUsername(newText)}
             value={usernameText}
           />
           <TextInput
-            style={styles.input}
+            style={page.input}
             secureTextEntry={true}
             placeholder="Enter your Password"
             onChangeText={(newText) => setTextPassword(newText)}
             value={passwordText}
           />
         </View>
-        <View style={styles.buttonWrapper}>
+        <View style={page.buttonWrapper}>
           <TouchableOpacity
-            style={styles.button}
+            style={page.button}
             onPress={() => Alert.alert("Login Button pressed")}
           >
-			<Text
-		  		style={styles.buttonText}
-		  	> 
-		  		Login 
-		  	</Text>
-		  </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => navigation.navigate("SignupScreen")}
-		  >
-		  	<Text
-		  		style={styles.buttonText}
-		  	> 
-		  		Sign up 
-		  	</Text>
+          <Text
+              style={page.buttonText}
+            > 
+              Login 
+            </Text>
           </TouchableOpacity>
+          <TouchableOpacity
+            style={page.button}
+            onPress={() => navigation.navigate("SignupScreen")}
+		      >
+          <Text
+            style={page.buttonText}
+          > 
+            Sign up 
+          </Text>
+            </TouchableOpacity>
         </View>
         <StatusBar style="auto" />
       </View>
-    </SafeAreaView>
+    </SafeAreaView> 
 	</LinearGradient>
   );
 }
-
-const styles = StyleSheet.create({
-  title: {
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 42,
-    fontFamily: "Roboto",
-	textAlign: 'center', 
-
-  },
-  button: {
-    alignItems: "center",
-	borderRadius: 5,
-    backgroundColor: "#F5D466",
-    justifyContent: "center",
-    marginVertical: "5%",
-    shadowColor: "#000",
-    shadowOffset: {
-     	width: 1,
-     	height: 3,
-    },
-    shadowOpacity: 0.50,
-    shadowRadius: 4,
-
-    elevation: 5,
-	width: 150,
-	height: 50, 
-  },
-  buttonText: {
-	fontFamily: "Roboto",
-	fontSize: 20,
-	fontWeight: "bold",
-	textAlignt: "center",
-	justifyContent: "center",
-	alignItems: "center",
-  }, 
-  text: {
-    color: "white",
-    fontSize: 26,
-    fontWeight: "300",
-    alignItems: "center",
-    fontFamily: "Roboto",
-	textAlign: "center", 
-  },
-  subtitle: {
-	//flex: 0.2,
-    color: "white",
-    fontSize: 26,
-    fontWeight: "300",
-    fontFamily: "Roboto",
-	justifyContent: "center",
-	alignItems: "center",
-	textAlign: "center",
-  },
-  ladida: {
-    //backgroundImage: "linear-gradient(#751A33, #B34233)",
-    height: "80%",
-    alignItems: "center",
-  },
-  container: {
-    flex: 1,
-    //backgroundImage: "linear-gradient(#751A33, #B34233)",
-	//backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  input: {
-    backgroundColor: "white",
-	fontSize: 21,
-    alignItems: "center",
-    justifyContent: "center",
-	textAlign: "center",
-    marginVertical: "3%",
-	marginHorizontal:"3%",
-    shadowColor: "#000",
-    shadowOffset: {
-     	width: 0,
-     	height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    borderRadius: 5,
-	height: 50,
-	width: 260,
-  },
-  inputWrapper: {
-	//borderWidth: 5,
-	flexDirection: 'column',
-    flex: 0.5,
-    //backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  buttonWrapper: {
-	//borderWidth: 10,
-	flexDirection: 'column', 
-    flex: 1,
-    //backgroundColor: "red",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  space: {
-	//backgroundColor: "white",
-    width: 100, // or whatever size  you need
-    height: 75, 
-  },
-});
