f32c81262548453976f6ee2858f1ab207dabbce1
Jesse Ravensbergen <jesse.ravensbergen@gmail.com>
2022-11-28 17:01:05 +0100

    finished new repair request screen

--- screens/NewRepairRequest.js
+++ screens/NewRepairRequest.js
@@ -10,27 +10,49 @@ import {
 import React, { useState } from "react";
 import {TouchableOpacity} from "react-native";
 import { useFonts } from 'expo-font';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function NewRepairRequest({ navigation }){
 
     const [description, onChangeText] = React.useState("Useless Text");
     const [amount, onChangeNumber] = React.useState(null);
     
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+      {label: 'Flat Tire', value: 'flattire'},
+      {label: 'Detached Chain', value: 'chain'},
+      {label: 'Other Issue', value: 'other'}
+
+    ]);
 
     return(
         <SafeAreaView style={styles.container}>
             <View style={styles.view}>
                 <Text style={styles.title}>BICARE</Text>
                 <Text style={styles.subtitle}>Barter your bike repair anywhere</Text>
+                <Text style={styles.header}>New Repair Request</Text>
                 <View style={styles.inputWrapper}>
                     <Text style={styles.fieldTitle}>Problem</Text>
-                    <Text style={styles.fieldTitle}>Price</Text>
-                    <TextInput
-                        style={styles.amountInput}
-                        placeholder="10"
-                        onChangeText={onChangeNumber}
-                        keyboardType="numeric"
+                    <DropDownPicker
+                      open={open}
+                      value={value}
+                      items={items}
+                      setOpen={setOpen}
+                      setValue={setValue}
+                      setItems={setItems}
                     />
+                    <Text style={styles.fieldTitle}>Price</Text>
+                    <View style={styles.amountView}>
+                      <Text style={styles.euroSign}>€</Text>
+                      <TextInput
+                          style={styles.amountInput}
+                          placeholder="10"
+                          onChangeText={onChangeNumber}
+                          keyboardType="numeric"
+                      />
+                    </View>
+
                     <Text style={styles.fieldTitle}>Description</Text>
                     <TextInput
                         style={styles.descriptionInput}
@@ -54,11 +76,19 @@ export default function NewRepairRequest({ navigation }){
 
 const styles = StyleSheet.create({
     title: {
-      color: "white",
+      color: "#F5D466",
       fontWeight: "bold",
       fontSize: 42,
       fontFamily: "Roboto",
   
+    },
+    header: {
+      paddingTop: 10,
+      textTransform: "uppercase",
+      color: "white",
+      fontSize: 20,
+      textDecorationLine: "underline",
+      fontWeight: "bold",
     },
     button: {
       alignItems: "center",
@@ -89,8 +119,9 @@ const styles = StyleSheet.create({
         fontSize: 18,
         alignItems: "center",
         justifyContent: "center",
-        marginTop: 5,
-        marginBottom: "5%",
+        alignSelf: "center",
+        //marginTop: 5,
+        //marginBottom: "5%",
         shadowColor: "#000",
         shadowOffset: {
              width: 0,
@@ -100,9 +131,24 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
         elevation: 5,
         borderRadius: 5,
+        flexGrow: 8,
         height: 30,
-        width: "80%",
+        maxWidth: 100,
+        paddingLeft: 5,
+        keyboardType: "number-pad",
       },
+    euroSign: {
+      color: "white",
+      //flex: 2,
+      alignSelf: "center",
+      fontSize: 20,
+      paddingRight: 5,
+    },
+    amountView: {
+      flexDirection: "row",
+      display: "flex",
+      alignSelf: "flex-start"
+    },
     descriptionInput: {
         //flex: 0.1,
         backgroundColor: "white",
@@ -119,7 +165,11 @@ const styles = StyleSheet.create({
         elevation: 5,
         borderRadius: 5,
         height: 180,
-        width: 250,
+        width: "100%",
+        textAlign: 'left',
+        textAlignVertical: 'top',
+        paddingTop: 5,
+        paddingHorizontal: 10,
       },
     text: {
       flex: 0.1,
@@ -131,7 +181,7 @@ const styles = StyleSheet.create({
       fontFamily: "Roboto",
     },
     fieldTitle: {
-        color: "black",
+        color: "white",
         fontSize: 14,
         fontWeight: "300",
         alignSelf: "flex-start",
@@ -139,19 +189,22 @@ const styles = StyleSheet.create({
       },
     subtitle: {
       //flex: 0.2,
-      color: "white",
+      color: "#F5D466",
       fontSize: 26,
       fontWeight: "300",
       fontFamily: "Roboto",
+      textAlign: "center"
     },
     view: {
       //backgroundImage: "linear-gradient(#751A33, #B34233)",
       backgroundSize: "cover",
       height: "80%",
+      width: "80%",
       alignItems: "center",
     },
     container: {
       flex: 1,
+      backgroundColor: "#27241A", //TODO: temporary, remove
       backgroundImage: "linear-gradient(#751A33, #B34233)",
       alignItems: "center",
       justifyContent: "center",
@@ -161,6 +214,7 @@ const styles = StyleSheet.create({
         gap: "5px",
         flexDirection: "column",
         flex: 1,
+        alignSelf: "stretch"
     },
     space: {
       //backgroundColor: "white",
