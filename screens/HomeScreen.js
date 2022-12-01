d95f9ed3d376b2a5d2f6ff8cbe476bfb86b02328
Alžbeta Hajná <betka.hajna@gmail.com>
2022-12-01 11:18:19 +0100

    Draft: HomeScreen to be able to navigate after loging

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //Work In Progress
 
 import {
@@ -32,3 +33,66 @@ export default function HomeScreen({ navigation }){
         </SafeAreaView>
     );
 }
+=======
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { auth } from '../src/firebase';
+
+export default function HomeScreen({ navigation }) {
+  const handleSignOut = () => {
+    auth
+      .signOut()
+      .then(() => {
+        navigation.navigate("LoginScreen")
+        console.log("signed out")
+      })
+      .catch(error => alert(error.message))
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Email: {auth.currentUser?.email}</Text>
+      <TouchableOpacity
+        onPress={handleSignOut}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Sign out</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    button: {
+      alignItems: "center",
+      borderRadius: 5,
+      backgroundColor: "#F5D466",
+      alignItems: "center",
+      justifyContent: "center",
+      marginVertical: "5%",
+      shadowColor: "#000",
+      shadowOffset: {
+           width: 1,
+           height: 3,
+      },
+      shadowOpacity: 0.50,
+      shadowRadius: 4,
+  
+      elevation: 5,
+      width: 150,
+      height: 50, 
+    },
+    buttonText: {
+      fontFamily: "Roboto",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    container: {
+      flex: 1,
+      backgroundImage: "linear-gradient(#751A33, #B34233)",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+});
+  
+>>>>>>> Draft: HomeScreen to be able to navigate after loging
