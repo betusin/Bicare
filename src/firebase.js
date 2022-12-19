88ae1a6e2b228551e17556dc7aa3e8b1919d40d9
Alžbeta Hajná <betka.hajna@gmail.com>
2022-12-19 16:44:32 +0100

    Draft: trying to solve warning about AsyncStorage, but still other warnings occurred

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore/lite';
 import { firebaseConfig } from './firebaseConfig';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import {getReactNativePersistence, initializeAuth} from 'firebase/auth/react-native';
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth();
+export const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+});
