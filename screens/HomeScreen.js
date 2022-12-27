bad70b94bcec6a38e07dfd4b4859ab8e84383e48
Alžbeta Hajná <betka.hajna@gmail.com>
2022-12-27 19:27:29 +0100

    style(home): format code

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -10,44 +10,44 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import page from '../styles';
-import {Image, TouchableOpacity} from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 
 
-export default function HomeScreen({ navigation }){
-    return(
-        <SafeAreaView style={page.container}>
-			<View style={page.view}>
-			<Image
-        		style={page.tinyLogo}
-        		source={require('../img/logoWhiteTrial2.png')}
-      		/>
-				<Text style={page.subtitle}>Barter your bike repair anywhere</Text>
-                <Text style={page.header}>Home Screen</Text>
-                <View style={page.buttonWrapper}>
-                <TouchableOpacity
-                    style={page.buttonProfile}
-                    onPress={() => navigation.navigate("New Repair Request")}
-                >
-                    <Text
-						style={page.buttonTextSmall}
-	    			> 
-                        New Repair Request 
-                    </Text>
-                </TouchableOpacity>
-            	
-                <TouchableOpacity
-                    style={page.buttonProfile}
-                    onPress={() => navigation.navigate("Change Status to fixer")}
-                >
-                    <Text
-						style={page.buttonTextSmall}
-	    			>
-						Change Status 
-                    </Text>
-				
-                </TouchableOpacity>
-                </View>
-			</View>
-        </SafeAreaView>
-    );
+export default function HomeScreen({ navigation }) {
+  return (
+    <SafeAreaView style={page.container}>
+      <View style={page.view}>
+        <Image
+          style={page.tinyLogo}
+          source={require('../img/logoWhiteTrial2.png')}
+        />
+        <Text style={page.subtitle}>Barter your bike repair anywhere</Text>
+        <Text style={page.header}>Home Screen</Text>
+        <View style={page.buttonWrapper}>
+          <TouchableOpacity
+            style={page.buttonProfile}
+            onPress={() => navigation.navigate("New Repair Request")}
+          >
+            <Text
+              style={page.buttonTextSmall}
+            >
+              New Repair Request
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={page.buttonProfile}
+            onPress={() => navigation.navigate("Change Status to fixer")}
+          >
+            <Text
+              style={page.buttonTextSmall}
+            >
+              Change Status
+            </Text>
+
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
