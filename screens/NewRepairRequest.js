67e590b4510506f5991c58e638d6af72a3765840
Alžbeta Hajná <betka.hajna@gmail.com>
2023-01-02 14:36:44 +0100

    feat: repair request saved with current location

--- screens/NewRepairRequest.js
+++ screens/NewRepairRequest.js
@@ -9,8 +9,8 @@ import {
   Image,
   TouchableOpacity
 } from "react-native";
-import React, { useState } from "react";
-import { useFonts } from 'expo-font';
+import React, { useEffect, useState } from "react";
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, Accuracy } from "expo-location";
 import DropDownPicker from 'react-native-dropdown-picker';
 import page from '../styles';
 import { addDoc, collection, GeoPoint, Timestamp } from "firebase/firestore";
@@ -20,6 +20,7 @@ export default function NewRepairRequest({ navigation }){
 
     const [description, onChangeText] = React.useState("Useless Text");
     const [amount, onChangeNumber] = React.useState(10);
+    const [location, setLocation] = useState({});
 
     const [open, setOpen] = useState(false);
     const [value, setValue] = useState(null);
@@ -30,6 +31,26 @@ export default function NewRepairRequest({ navigation }){
 
     ]);
 
+    useEffect(() => {
+        (async () => {
+            let { status } = await requestForegroundPermissionsAsync();
+            if (status !== "granted") {
+            return;
+            }
+            // Gets the current location from expo
+            try {
+            let location = await getCurrentPositionAsync({
+                accuracy: Accuracy.Balanced,
+                enableHighAccuracy: true,
+                timeInterval: 5,
+            });
+            setLocation(location);
+            } catch (error) {
+            alert(error);
+            }
+        })();
+    }, []);
+
     const createRepairRequest = () => {
         const user = auth.currentUser;
         var selectedItem = items.filter((item) => item.value == value)[0];
@@ -38,7 +59,7 @@ export default function NewRepairRequest({ navigation }){
             description: description,
             amount: Number(amount),
             requester: user.uid,
-            location: new GeoPoint(90, 90),
+            location: new GeoPoint(location.coords.latitude, location.coords.longitude),
             createdAt: Timestamp.fromDate(new Date()),
         };
 
